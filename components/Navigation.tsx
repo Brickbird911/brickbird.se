5dd287b8aba53dc8d18545c086fbc4a9406695c4
Brickbird <105644104+Brickbird911@users.noreply.github.com>
2023-09-12 19:43:06 +0000

    jk

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -2,7 +2,7 @@ import NavigationItem from "./NavigationItem";
 
 export default function Navigation() {
 	return (
-		<nav className='bg-black px-3 py-4 rounded-3xl m-2 flex flex-row items-center'>
+		<nav className='bg-black px-1 py-1 rounded-3xl m-2 flex flex-row items-center'>
 			<NavigationItem name='Home' link='/' />
 			<div className='flex-1'></div>
 			<NavigationItem name='About' link='/about' />
--- components/NavigationItem.tsx
+++ components/NavigationItem.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function NavigationItem({ name = '', link = '' }) {
 	return (
-		<Link href={link} className='text-white bg-black hover:bg-cyan-700 p-2 rounded-xl'>
+		<Link href={link} className='text-white bg-black hover:bg-cyan-700 p-2 rounded-3xl'>
 			<span>{name}</span>
 		</Link>
 	);
