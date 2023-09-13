0cd845719d3424549e49c94d02840bcdeab78e64
Brickbird <105644104+Brickbird911@users.noreply.github.com>
2023-09-13 10:00:41 +0000

    k

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,17 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
+import { Changa_One, Changa_One } from 'next/font/google'
+
+const Changa_One = Changa_One({ subsets: ['latin'] })
+ 
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <main className={Changa_One.className}>
+      <Component {...pageProps} />
+    </main>
+  )
+}
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
