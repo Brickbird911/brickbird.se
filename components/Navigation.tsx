304842c96faad17e5cec30f4efc0593ab21671f4
Brickbird <105644104+Brickbird911@users.noreply.github.com>
2023-09-13 13:11:51 +0000

    d

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -2,8 +2,8 @@ import NavigationItem from "./NavigationItem";
 
 export default function Navigation() {
 	return (
-		<nav className='bg-black px-1 py-1 rounded-3xl m-2 flex flex-row items-center'>
-			<NavigationItem name='Home' link='/' />
+		<nav className={'bg-black px-1 py-1 rounded-3xl m-2 flex flex-row items-center'}>
+			<NavigationItem name='Brickbird' link='/' changa /> 
 			<div className='flex-1'></div>
 			<NavigationItem name='About' link='/about' />
 			<NavigationItem name='Logs' link='/log' />
--- components/NavigationItem.tsx
+++ components/NavigationItem.tsx
@@ -1,8 +1,15 @@
 import Link from "next/link";
 
-export default function NavigationItem({ name = '', link = '' }) {
+import { Changa_One } from 'next/font/google'
+
+const changaOne = Changa_One({
+	subsets: ['latin'],
+	weight: '400'
+});
+
+export default function NavigationItem({ name = '', link = '', changa = false }) {
 	return (
-		<Link href={link} className='text-white bg-black hover:bg-cyan-700 p-2 rounded-3xl'>
+		<Link href={link} className={'text-white bg-black hover:bg-cyan-700 p-2 rounded-3xl ' + (changa ? changaOne.className : '')}>
 			<span>{name}</span>
 		</Link>
 	);
