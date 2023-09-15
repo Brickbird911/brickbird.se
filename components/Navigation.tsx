872db3acfc2fc43701cabd73990aaeef85c21c21
Brickbird <105644104+Brickbird911@users.noreply.github.com>
2023-09-15 09:31:34 +0000

    g

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -2,7 +2,7 @@ import NavigationItem from "./NavigationItem";
 
 export default function Navigation() {
 	return (
-		<nav className={'bg-slate-400 px-1 py-1 rounded-3xl m-2 flex flex-row items-center'}>
+		<nav className={'bg-slate-200 flex flex-row items-center'}>
 			<NavigationItem name='Brickbird' link='/' changa /> 
 			<div className='flex-1'></div>
 			<NavigationItem name='About' link='/about' />
--- pages/log.tsx
+++ pages/log.tsx
@@ -3,6 +3,7 @@ import Navigation from "@/components/Navigation"
 export default function Log() {
     return(
         <main>
+            <Navigation />
             <a>HEJ</a>
         </main>
     )
