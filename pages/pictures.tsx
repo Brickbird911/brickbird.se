957fc1e897bd8c0a74db367877ce5b64ef311353
Brickbird <105644104+Brickbird911@users.noreply.github.com>
2023-09-26 12:05:15 +0000

    d

---
+++ pages/pictures.tsx
@@ -0,0 +1,11 @@
+import Background from "@/components/Background"
+import Navigation from "@/components/Navigation"
+
+ export default function Pictures() {
+	return(
+		<main>
+		 <Navigation />
+		 <Background />
+		 </main>
+	)
+ }
