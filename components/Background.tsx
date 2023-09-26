5f9ff1236cea2a61f619d1a04957e7b3ed55bfd9
Brickbird <105644104+Brickbird911@users.noreply.github.com>
2023-09-26 08:45:31 +0000

    isak

--- components/Background.tsx
+++ components/Background.tsx
@@ -7,19 +7,19 @@ export default function Background(){
 		<h1>Aviation</h1>
 		<div className="flex flex-row m-2">
 			<Link href="/img/plane1">
-				<Image alt='plane' src="/DSC_0586.png" width={400} height={265} />
+				<Image alt='plane' src="/plane1.png" width={400} height={265} />
 			</Link>
 			<Link href="/img/plane2">
-				<Image alt='plane' src="/DSC_0559.png" width={400} height={265} />
+				<Image alt='plane' src="/plane2.png" width={400} height={265} />
 			</Link>
 			<Link href="/img/plane3">
-				<Image alt="plane" src="/DSC_0789.png" width={400} height={265}/>
+				<Image alt="plane" src="/plane3.png" width={400} height={265}/>
 			</Link>
 		</div>
 		<h1>Nature</h1>
 		<div>
 			<Link href="/img/nature1">
-				<Image alt="nature" src="/DSC_0444-(1).png" width={400} height={265}/>
+				<Image alt="nature" src="/nature1.png" width={400} height={265}/>
 			</Link>
 		</div>
 	</main>
