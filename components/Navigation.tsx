9ee240b77a7043c77e48d1896c6c59898196eeab
Brickbird.development <105644104+Brickbird@users.noreply.github.com>
2023-09-30 14:49:41 +0200

    d

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -4,7 +4,7 @@ export default function Navigation() {
 	return (
 		<nav className={'bg-slate-200 flex flex- items-center rounded-full m-2'}>
 			<NavigationItem name= 'Brickbird' link='/' changa /> 
-			<NavigationItem name='About' link='/about' />
+			<NavigationItem name='Pictures' link='/pictures' />
 			<NavigationItem name='Logs' link='/log' />
 		</nav>
 	);
