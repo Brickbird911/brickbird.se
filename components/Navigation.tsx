99b361865bfb00ae246fddf3684c102c588a80f3
Brickbird.development <105644104+Brickbird@users.noreply.github.com>
2023-10-08 18:49:57 +0000

    v

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -2,7 +2,7 @@ import NavigationItem from "./NavigationItem";
 
 export default function Navigation() {
 	return (
-		<nav className={'bg-[#3B4252] flex flex- items-center'}>
+		<nav className={'rounded-b- bg-[#3B4252] flex flex- items-center'}>
 			<NavigationItem name= 'Brickbird' link='/' changa /> 
 			<NavigationItem name='Pictures' link='/pictures' />
 			<NavigationItem name='Logs' link='/log' />
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Navigation from "@/components/Navigation"
 import Background from "@/components/Background";
 import Colorsindex from "@/components/Colorsindex";
+import Image from "next/image";
 
 export default function Index() {
 	return (
