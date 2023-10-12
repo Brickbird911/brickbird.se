a7aac4ec116ce871c1296b1e92f39d8535092375
Brickbird~ <ulw3087@edu.kunskapsskolan.se~>
2023-10-12 21:11:19 +0200

    d

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,9 @@ import Indexcomp from "@/components/Indexcomp"
 
 export default function Index() {
 	return(
-		<><Navigation /><Indexcomp /></>
+		<main>
+			<Navigation />
+			<Indexcomp />
+		</main>
 	)
-}
+} 
