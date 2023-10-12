b272922629ab5d25e93c5a60619a565e57dd7614
Brickbird~ <ulw3087@edu.kunskapsskolan.se~>
2023-10-12 21:05:57 +0200

    hs

---
+++ components/Indexcomp.tsx
@@ -0,0 +1,7 @@
+export default function Indexcomp() {
+	return(
+		<main className="bg-[#2e3440]">
+
+		</main>
+	)
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,9 @@
 import Navigation from "@/components/Navigation"
+import Indexcomp from "@/components/Indexcomp"
+
 export default function Index() {
 	return(
 		<Navigation />
+		<Indexcomp />
 	)
 }
