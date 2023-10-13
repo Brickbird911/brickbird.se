22baa3aadefc52d32e0dab8582079db2ac21a9c4
Brickbird~ <ulw3087@edu.kunskapsskolan.se~>
2023-10-13 20:35:54 +0200

    d

--- components/Indexcomp.tsx
+++ components/Indexcomp.tsx
@@ -1,10 +1,15 @@
 export default function Indexcomp() {
 	return(
-		<main className="h-screen bg-[#2e3440]">
-			<div className="flex flex-row-reverse mr-[200px] mt-[100px] text-white font-bold">
+		<main className="h-screen bg-[#371B58]">
+			<div className="">
+			<button>
+				<audio src="halloween23.mp3" controls autoPlay/>
+			</button>
+			</div>
+			<div className="flex flex-row-reverse mr-[200px] mt-[100px] text-[#F6635C] font-bold">
 				<a>This website is developed using React and designed with Tailwind css</a>
 			</div>
-			<div className="flex flex-row-reverse mr-[170px] text-white">
+			<div className="flex flex-row-reverse mr-[170px] text-[#F6635C]">
 				If you have any tips or thing i should add send it in me and my friends discord
 			</div>
 		</main>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,10 @@
 import Navigation from "@/components/Navigation"
 import Indexcomp from "@/components/Indexcomp"
 
+
 export default function Index() {
 	return(
-		<main className="h-screen bg-[#2e3440]">
+		<main className="h-screen bg-[#371B58]">
 			<Navigation />
 			<Indexcomp />
 		</main>
