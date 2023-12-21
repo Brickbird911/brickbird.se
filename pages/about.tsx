304bb23b0e5411067a0aab75320f2c25b4113e07
Brickbird911 <105644104+Brickbird911@users.noreply.github.com>
2023-12-21 14:55:58 +0000

    b

---
+++ pages/about.tsx
@@ -0,0 +1,9 @@
+export default function About() {
+	return(
+		<main>
+			<div>
+				<a>Hi</a>
+			</div>
+		</main>
+	)
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,8 @@ export default function Index() {
 	return (
 		<main className="bg-[#191724] w-screen h-screen m-0 p-0 overflow-hidden">
 			<div className="m-3 text-white">
-				<a href="/pages/index.tsx">Home</a>
+				<a href="/">Home</a>
+				<a href="about">About</a>
 			</div>
 		</main>
 	);
