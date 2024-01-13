aef581aa508c5a7d4675c4b99599e8d5aba3b70c
Brickbird911 <105644104+Brickbird911@users.noreply.github.com>
2024-01-13 22:00:16 +0000

    renamed some stuff

--- components/Topnav.tsx
+++ components/Topnav.tsx
@@ -1,8 +1,8 @@
 import Link from "next/link";
 
-export default function Nav () {
+export default function topnav () {
 	return (
-		<div className="w-screen flex items-center h-16 text-white bg-[#161425]">
+		<div className="w-screen flex items-center h-16 text-black dark:text-white bg-slate-200 dark:bg-[#161425]">
 			<Link className="ml-5" href="/">Home</Link>
 			<div className="ml-auto order-2 mr-5">
 				<Link href="/about">About</Link>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,9 @@
-import Nav from "@/components/Nav";
-import Image from "next/image";
-import Link from "next/link";
+import Topnav from "@/components/Topnav";
 
 export default function Index() {
 	return(
-		<main className="w-screen h-screen bg-[#080713] p-0 m-0">
-			<Nav />
+		<main className="w-screen h-screen bg-white dark:bg-[#080713] p-0 m-0">
+			<Topnav />
 			<div className='w-screen h-5/6 flex items-center justify-center'>
 				<div className='w-4/6 h-5/6 bg-slate-100 dark:bg-[#1f1d2e] text-black dark:text-white flex rounded-lg'>
 					<div className='m-3'>
