9b4071ff4cb79265977bae72f6748e93804186fd
Brickbird911 <105644104+Brickbird911@users.noreply.github.com>
2024-01-17 10:35:39 +0000

    h

--- components/Topnav.tsx
+++ components/Topnav.tsx
@@ -5,9 +5,11 @@ export default function topnav () {
 	return (
 		<div className="w-screen flex items-center h-16 text-black dark:text-white bg-slate-200 dark:bg-[#161425]">
 			<Link className="ml-5" href="/">Home</Link>
-			<div className="align-center">
+			<div className="flex-1"></div>
+			<div className="flex">
 				<Image src="/android-chrome-512x512.png" alt="logo"></Image>
 			</div>
+			<div className="flex-1"></div>
 			<div className="ml-auto order-2 mr-5">
 				<Link href="/about">About</Link>
 			</div>
