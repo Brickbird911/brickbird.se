14dc3e7d84ceb985523fc970fa1e5f7f712f5b66
Brickbird911 <105644104+Brickbird911@users.noreply.github.com>
2024-01-30 21:06:22 +0000

    feat:Added portalhbu links to all the different pages

--- pages/portalhub.tsx
+++ pages/portalhub.tsx
@@ -1,9 +1,25 @@
 import Topnav from "@/components/Topnav"
+import Link from "next/link"
 
 export default function portalhub () {
 	return (
 		<main className="bg-[]">
-			<Topnav />		
+			<Topnav />
+			<div className="flex justify-center align-middle font-medium mt-4">
+				<div className="border-2">
+					<Link href="/">Home</Link>
+				</div>
+			</div>
+			<div className="flex justify-center align-middle font-medium mt-2">
+				<div className="border-2">
+					<Link href="/photgraphy">Photo album</Link>
+				</div>
+			</div>
+			<div className="flex justify-center align-middle font-medium mt-2">
+				<div className="border-2">
+					<Link href="/about">About</Link>
+				</div>
+			</div>
 		</main>
 	)
 }
