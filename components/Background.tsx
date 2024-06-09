3e443712fc3ed5bdea45357d74ed975df61608e3
Brickbird911 <105644104+Brickbird911@users.noreply.github.com>
2024-06-09 16:12:31 +0000

    removed

--- components/Background.tsx
+++
@@ -1,9 +0,0 @@
-import Topnav from "./Topnav"
-
-export default function Bg () {
-	return (
-		<div className="bg-black w-screen h-screen">
-			<Topnav />
-		</div>
-	)
-}
--- pages/wallpapers.tsx
+++
@@ -1,9 +0,0 @@
-import Topnav from "@/components/Topnav"
-
-export default function wallpapers () {
-	return (
-		<main className="w-screen h-screen bg-white dark:bg-[#080713]">
-			<Topnav />
-		</main>
-	)
-}
